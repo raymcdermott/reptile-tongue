f55ef1f737e58a9926b0d28cc256471ab93b2e31
ray.mcdermott <ray@mcdermott.be>
2018-03-27 00:59:32 +0200

    stash with color

--- src/cljs/repl_ui/views.cljs
+++ src/cljs/repl_ui/views.cljs
@@ -9,33 +9,20 @@
 
 (def default-style {:font-family "Menlo, Lucida Console, Monaco, monospace"
                     :border      "1px solid lightgray"
-                    :padding     "0px 20px 0px 20px"})
+                    :padding     "10px 20px 0px 20px"})
 
 (def panel-style (merge (flex-child-style "1")
-                        default-style
-                        {:background-color "#e8e8e8"}))
+                        default-style {:background-color "#e8e8e8"}))
 
 (def input-style (merge (flex-child-style "1")
-                        default-style
-                        {:resize "none"}))
+                        default-style {:resize "none"}))
 
 (def eval-style (merge (flex-child-style "1") default-style))
 
-(defn heading []
-  (let [name (re-frame/subscribe [::subs/name])]
-    [title :label (str "REPtiLe by " @name)
-     :level :level4
-     :style {:margin-left "20px"}]))
+(defn splitter-panel-title [text]
+  [title :label text :level :level3 :style {:margin-top "20px"}])
 
-(defn splitter-panel-title
-  [text]
-  [title
-   :label text
-   :level :level3
-   :style {:margin-top "20px"}])
-
-(defn read-input-panel
-  [user-name]
+(defn read-input-panel [user-name]
   (let [current-form @(re-frame/subscribe [::subs/user-keystrokes user-name])]
     [v-box :size "auto" :children
      [[box :size "auto" :child
@@ -43,35 +30,24 @@
         [label :label current-form]
         [splitter-panel-title [:code user-name]]]]]]))
 
-(defn format-result
-  [result]
-  (list [label :label (:form result)]
-        [label :label (str "=> " (:val result)) :on-click #(js/alert result)]
-        [gap :size "10px"]))
+(defn format-result [result]
+  (let [form (or (first (edn/read-string (:pretty result))) (:form result))]
+    (list [label :label form]
+          [label :label (str "=> " (:val result)) :on-click #(js/alert result)]
+          [gap :size "10px"])))
 
-(defn eval-panel
-  []
-  (#_ [box :size "auto" :child
-    [:div (list [:span {:style "font-weight:bold;color:red"} "("]
-                [:span {:style "font-weight:bold;color:blue"} "+"]
-                " "
-                [:span {:style "color:cyan"} "1"]
-                " "
-                [:span {:style "color:cyan"} "2"]
-                [:span {:style "font-weight:bold;color:red"} ")"])]])
-
+(defn eval-panel []
   (let [eval-results @(re-frame/subscribe [::subs/eval-results])]
-       [scroller :child
-        ; TODO make CSS dynamic so that we don't lose it when scrolling
-        [box :size "auto" :child
-         [:div {:style eval-style}
-          (when eval-results
-            [v-box :size "auto" :children
-             (map format-result eval-results)])]]]))
+    [scroller :child
+     ; TODO make CSS dynamic so that we don't lose it when scrolling
+     [box :size "auto" :child
+      [:div {:style eval-style}
+       (when eval-results
+         [v-box :size "auto" :children
+          (map format-result eval-results)])]]]))
 
-(defn edit-panel
-  [panel-name]
-  (let [user-name     @(re-frame/subscribe [::subs/user-name])
+(defn edit-panel [panel-name]
+  (let [user-name @(re-frame/subscribe [::subs/user-name])
         parinfer-form @(re-frame/subscribe [::subs/parinfer-form])]
     [scroller :child
      [v-box :size "auto" :children
@@ -89,28 +65,18 @@
         :on-click #(re-frame/dispatch
                      [::events/eval (.-value (.getElementById js/document panel-name))])]]]]))
 
-(defn login-panel
-  []
-  [h-box
-   :size "auto"
-   :children
-   [[input-text
-     :model ""
-     :placeholder "user-name"
-     :style input-style
-     :attr {:id "login-panel"}
+(defn login-panel []
+  [h-box :size "auto" :children
+   [[input-text :model "" :placeholder "user-name" :style input-style :attr {:id "login-panel"}
      :on-change #(-> nil)]
-    [button
-     :label "Login"
+    [button :label "Login"
      :on-click #(re-frame/dispatch
                   [::events/login (.-value (.getElementById js/document "login-panel"))])]]])
 
-(defn box-panels
-  []
-  (let [user-name     @(re-frame/subscribe [::subs/user-name])
+(defn box-panels []
+  (let [user-name @(re-frame/subscribe [::subs/user-name])
         other-editors @(re-frame/subscribe [::subs/other-editors user-name])]
-    [h-split
-     :margin "2px"
+    [h-split :margin "2px"
      :panel-1 [v-split
                :panel-1 [read-input-panel (or (nth other-editors 0 "0"))]
                :panel-2 (if user-name
@@ -121,11 +87,7 @@
                :panel-2 [read-input-panel (or (nth other-editors 2 "2"))]]]))
 
 (defn main-panel []
-  [v-box
-   :height "800px"
-   :children [[v-split
-               :splitter-size "3px"
-               :initial-split "60%"
-               :panel-1 [eval-panel]
-               :panel-2 [box-panels]]
-              [heading]]])
+  [v-box :height "800px" :children
+   [[v-split :splitter-size "3px" :initial-split "60%"
+     :panel-1 [eval-panel]
+     :panel-2 [box-panels]]]])
