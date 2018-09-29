b25b46afae11156bf4552f470fd403808eeb027c
Ray McDermott <ray@mcdermott.be>
2018-09-29 17:45:56 +0200

    :bug: fix to support add-lib via DynamicClassLoader

--- src/reptile/tongue/server.clj
+++ src/reptile/tongue/server.clj
@@ -1,9 +1,14 @@
 (ns reptile.tongue.server
   (:require [reptile.server.http :as http]))
 
-(let [port   8888
-      secret "warm-blooded-lizards-rock"]
-  (println "Starting server")
-  (http/start-reptile-server port secret))
+(try
+  ; Need DynamicClassLoader to support add-lib
+  (let [port           8888
+        secret         "warm-blooded-lizards-rock"
+        current-thread (Thread/currentThread)
+        cl             (.getContextClassLoader current-thread)]
+    (.setContextClassLoader current-thread (clojure.lang.DynamicClassLoader. cl))
+    (http/start-reptile-server port secret))
+  (catch Exception e (str "ClassLoader issue - caught exception: " (.getMessage e))))
 
 (defn handler [request] request)
