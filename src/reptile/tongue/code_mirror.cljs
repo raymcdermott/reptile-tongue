4c08469fedfbc31971147ffbc4fb8aac4eb2e43c
Ray McDermott <ray@mcdermott.be>
2018-10-17 23:41:57 +0200

    :bug: cm width / height reversed

--- src/reptile/tongue/code_mirror.cljs
+++ src/reptile/tongue/code_mirror.cljs
@@ -20,9 +20,9 @@
   [dom-node config]
   (let [editor-options (clj->js (merge {:mode :clojure} (:options config)))
         code-mirror    (js/CodeMirror.fromTextArea dom-node editor-options)
-        editor-height  (get-in config [:size :height] "100%")
-        editor-width   (get-in config [:size :width] "100%")]
-    (.setSize code-mirror editor-height editor-width)
+        editor-width   (get-in config [:size :width] "100%")
+        editor-height  (get-in config [:size :height] "100%")]
+    (.setSize code-mirror editor-width editor-height)
     (js/parinferCodeMirror.init code-mirror)
     code-mirror))
 
