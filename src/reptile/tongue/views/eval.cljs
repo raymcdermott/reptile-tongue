b05cd9ddce4ea2b176dcdb44d8f1519b3e1cdd24
Ray McDermott <ray@mcdermott.be>
2018-10-17 23:39:35 +0200

    :bug: eval scrolling is back

--- src/reptile/tongue/views/eval.cljs
+++ src/reptile/tongue/views/eval.cljs
@@ -1,21 +1,20 @@
 (ns reptile.tongue.views.eval
   (:require
     [re-frame.core :as re-frame]
-    [re-com.core :refer [h-box v-box box button gap line scroller border label input-text md-circle-icon-button
-                         md-icon-button input-textarea modal-panel h-split v-split title flex-child-style
-                         radio-button p]]
-    [re-com.splits :refer [hv-split-args-desc]]
+    [re-com.core :refer [h-box v-box box button gap line border label input-text
+                         md-circle-icon-button md-icon-button input-textarea modal-panel
+                         h-split v-split title flex-child-style radio-button p]]
     [reagent.core :as reagent]
     [reptile.tongue.subs :as subs]
     [reptile.tongue.events :as events]
     [reptile.tongue.code-mirror :as code-mirror]))
 
-(def default-style {:font-family "Menlo, Lucida Console, Monaco, monospace"
-                    :border      "1px solid lightgray"
-                    :padding     "5px 5px 5px 5px"})
-
 (def eval-panel-style (merge (flex-child-style "1")
-                             default-style))
+                             {:border  "1px solid lightgray"
+                              :padding "5px 5px 5px 5px"}))
+
+(def eval-component-style (merge (flex-child-style "1")
+                                 {:padding "15px 5px 0px 5px"}))
 
 (defn eval-did-mount
   []
@@ -44,23 +43,25 @@
     []
     (let [show-times? @(re-frame/subscribe [::subs/show-times])]
       [v-box :size "auto" :style eval-panel-style
-       :children [[h-box :align :center :justify :end :gap "20px" :height "20px"
-                   :children [[md-icon-button
-                               :tooltip "Show evaluation times"
-                               :size :smaller
-                               :md-icon-name "zmdi-timer"
-                               :style {:color (if show-times? "red" "black")}
-                               :on-click #(re-frame/dispatch [::events/show-times (if show-times?
-                                                                                    (false? show-times?)
-                                                                                    true)])]
-                              [md-icon-button
-                               :tooltip "Clear evaluations"
-                               :size :smaller
-                               :md-icon-name "zmdi-delete"
-                               :on-click #(re-frame/dispatch [::events/clear-evals])]
-                              [md-icon-button
-                               :tooltip "Wrap text (default ON)"
-                               :size :smaller
-                               :md-icon-name "zmdi-wrap-text"]]]
-                  [box :size "auto"
-                   :child [eval-component panel-name]]]])))
+       :children
+       [[h-box :align :center :justify :end :gap "20px" :height "20px"
+         :children
+         [[md-icon-button
+           :tooltip "Show evaluation times"
+           :size :smaller
+           :md-icon-name "zmdi-timer"
+           :style {:color (if show-times? "red" "black")}
+           :on-click #(re-frame/dispatch [::events/show-times (if show-times?
+                                                                (false? show-times?)
+                                                                true)])]
+          [md-icon-button
+           :tooltip "Clear evaluations"
+           :size :smaller
+           :md-icon-name "zmdi-delete"
+           :on-click #(re-frame/dispatch [::events/clear-evals])]
+          [md-icon-button
+           :tooltip "Wrap text (default ON)"
+           :size :smaller
+           :md-icon-name "zmdi-wrap-text"]]]
+        [box :size "auto" :style eval-component-style
+         :child [eval-component panel-name]]]])))
